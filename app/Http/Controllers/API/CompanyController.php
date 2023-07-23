1443c9ee1594b2fc8551cfcbf4ad1e073855714e
Fajar Octhaviano <fajarduaa@gmail.com>
2023-07-23 22:21:45 +0700

    Seeder & Factories, Many to Many Relationship

---
+++ app/Http/Controllers/API/CompanyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+
+class CompanyController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $name = $request->input('name');
+        $limit = $request->input('limit', 10);
+
+        // domain.com/api/company?id=1
+        if ($id) {
+            $company = Company::with(['users'])->find($id);
+            if ($company) {
+                return ResponseFormatter::success($company, 'Company Found');
+            }
+            return ResponseFormatter::error('Company Not Found', 404);
+        }  
+
+        // domain.com/api/company
+        $companies = Company::with(['users']);
+
+        if ($name) {
+            $companies->where('name', 'like', '%' . $name . '%');
+        }
+        
+        return ResponseFormatter::success(
+            $companies->paginate($limit),
+            'Companies found'
+        );
+
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\Team;
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +24,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Company::factory(10)->create();
+        // Team::factory(30)->create();
+        // Role::factory(20)->create();
+        // Responsibility::factory(200)->create();
+         Employee::factory(1000)->create();
     }
 }
