f16f3296011056eb67175ee4097532e7a8d07871
Fajar Octhaviano <fajarduaa@gmail.com>
2023-07-28 15:18:36 +0700

    Update Route API

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/company', [CompanyController::class, 'all']);
 
 Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
